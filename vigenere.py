17083511016636a7b34c332677a252dc08261e93
Zimmerwine <kakakecil67@gmail.com>
2025-03-28 01:18:03 +0700

    first commit

--- vigenere.py
+++ vigenere.py
@@ -14,26 +14,33 @@ def vigenere(text, key, mode = 1):
     for i, char in enumerate(text):
         if char.isalpha():
             new_key = ord(key_expanded[key_index].upper()) - ord('A')
-            if mode == 1:
-                shift = new_key
-            else: 
-                shift = -new_key
+            shift = new_key if mode == 1 else -new_key
             if char.isupper():
                 result += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
             elif char.islower():
                 result += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            key_index += 1
         else:
             result += char
-        key_index += 1
 
     return result
 
-text = str(input('Input your plain text here : '))
-key = str(input('Input your key here : '))
-encrypted = vigenere(text, key)
-print(f'text : {text}, cipher : {encrypted}')
-decrypted = vigenere(encrypted, key, -1)
-print(f'cipher : {encrypted}, text = {decrypted}')
+while True:
+    print('1. Encrypt')
+    print('2. Decrypt')
+    choice = int(input('Choose an option : '))
+    if choice == 1:
+        text = str(input('Input your plain text here : '))
+        key = str(input('Input your key here : '))
+        encrypted = vigenere(text, key, mode=1)
+        print(f'text : {text}, cipher : {encrypted}')
+    elif choice == 2:
+        cipher = str(input('Input your cipher here : '))
+        key = str(input('Input your key here : '))
+        decrypted = vigenere(cipher, key, mode=-1)
+        print(f'Cipher : {cipher}, text = {decrypted}')
+    else:
+        print('Your input is invalid, Try Again')    
     
 
        
